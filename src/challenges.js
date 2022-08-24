3407f775a508df902a85201fcbd56efae7698eca
Vinicius <vinicius2197@icloud.com>
2022-08-24 20:42:56 -0300

    desafios 1, 2 e 3 resolvidos

--- src/challenges.js
+++ src/challenges.js
@@ -1,17 +1,24 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(valueUm, valueDois) {
+  if (valueUm && valueDois) {
+    return true;
+  } else return false
 }
 
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  let area = (base * height ) / 2
+  return area;
 }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+
+
+function splitSentence(palavra) {
+    let array = palavra.split(" ");
+    return array;
 }
+
 
 // Desafio 4
 function concatName() {
